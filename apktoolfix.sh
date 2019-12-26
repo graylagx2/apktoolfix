d695ab222fc801d23ba2eab56f78dbb25d586d0a
graylagx2 <50938347+graylagx2@users.noreply.github.com>
2019-12-26 11:11:10 -0800

    Update apktoolfix.sh

--- apktoolfix.sh
+++ apktoolfix.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash/env bash 
 
 #Author: graylagx2
 #Name: apktoolfix
@@ -6,11 +6,16 @@
 #Description: Fix for apktool in kali linux.
 #Contact: graylagx2@gmail.com
 
+_SILENT_JAVA_OPTIONS="$_JAVA_OPTIONS"
+unset _JAVA_OPTIONS
+alias java='java "$_SILENT_JAVA_OPTIONS"'
+
 RED=$'\e[1;31m'
 GREEN=$'\e[1;32m'
 YELLOW=$'\e[1;33m'
 BLUE=$'\e[1;34m'
 RESTORE=$'\e[0m'
+L_GREY=$'\e[0;37m'
 
 spinLoader() {
     pid=$!
@@ -22,7 +27,8 @@ spinLoader() {
         sleep .1
     done
     printf "\r ${GREEN}[-]${RESTORE} $COMP_MESSAGE"
-    sleep 1;echo 
+    sleep 1;echo java_V=$(java -version 2>&1 | awk 'NR == 1 { gsub("\"",""); print $3}' | awk -F. '{print $1"."$2}')
+
 }
 
 echo -e "${BLUE}"
@@ -33,74 +39,66 @@ echo '
         |_______|__| |___._|___  |_______|___._|___  |__.__|______|
                            |_____|             |_____|             
 '                                                                     
-echo -e "${YELLOW}                           Apktool-Fix Version ${BLUE}2.0${RESTORE}"
-echo
+echo -e "${YELLOW}                           Apktool-Fix Version ${BLUE}2.1${RESTORE}\n"
+echo -e "${L_GREY}This script was developed to be used with the kali-linux distribution any use outside of this distribution may not work${RESTORE}\n"
+sleep 2
                                                       
 # Checking internet connection to exit with message if none
 [[ $(wget -q --tries=5 --timeout=20 --spider http://google.com ; echo $?) != 0 ]] && echo -e "${RED}Warning!${YELLOW} This script needs an internet connection!" && echo && echo -e "${YELLOW}Please connect to the internet and try again.${RESTORE}" && exit
 
 # Checking for for missing dependencies and install missing
-    echo -e "\n${BLUE}[-] ${YELLOW}Checking Dependencies${RESTORE}\n"
-    mapfile -t pkg_depends < <(apt-cache depends apktool | cut -d':' -f2 | sed  's/^[ \t]*//;/<java7-runtime-headless>/d;/^apktool\b/d;/libcommons-io-java/d;/libcommons-lang3-java/d;/libguava-java/d;/libstringtemplate-java/d;/libxmlunit-java/d' | sort -u)
+echo -e "\n${BLUE}[-] ${YELLOW}Checking Dependencies${RESTORE}\n";
+
+# Checking the current version of java thats installed
+java_V=$(java -version 2>&1 | awk 'NR == 1 { gsub("\"",""); print $3}' | awk -F. '{print $1"."$2}' 2>/dev/null)
+# Testing if java version is equal or greater than 1.8
+declare $(awk -v version="$java_V" 'BEGIN{if(version>1.8){ print "java_Met=true"}}')
+if [ "$java_Met" == "true" ]; then     
+    sleep 0.5;echo -e "${GREEN}  [-] ${YELLOW} Java-Version 1.8 or greater is ${GREEN}Installed.${RESTORE}"
+    mapfile -t pkg_depends < <(apt-cache depends apktool | cut -d':' -f2 | sed '/^apktool\b/d;/^i386\b/d;/headless/d')
     pwd=$(pwd)
+    
     for depend in "${pkg_depends[@]}"; do 
         pkg_qry=$(dpkg-query -s $depend &>/dev/null ; echo $?)
         if [ $pkg_qry = 0 ]; then
-            echo -e "${YELLOW}$depend is ${GREEN}Installed.${RESTORE}"
+            sleep 0.5;echo -e "${GREEN}  [-] ${YELLOW}$depend is ${GREEN}Installed.${RESTORE}"
         else 
-            echo -e "${YELLOW}$depend is ${RED}missing.${RESTORE}"
+            sleep 0.5;echo -e "${RED}  [-] ${YELLOW}$depend is ${RED}missing.${RESTORE}"
             MISSING+=($depend)
             [[ -e /tmp/repair_depends ]] || mkdir /tmp/repair_depends
             cd /tmp/repair_depends && apt-get download $depend &>/dev/null
             cd $pwd     
         fi
-    done  
+    
+    done
 
     if [[ ! -z $MISSING ]]; then 
-        [[ "${MISSING[*]} " != *"aapt"* ]] && [[ "${MISSING[*]} " == *"google-android-build-tools-installer"* ]] && MISSING=( "${MISSING[@]/google-android-build-tools-installer/}" ) && rm /tmp/repair_depends/google-android-build-tools-installer*
-        [[ "${MISSING[*]} " != *"google-android-build-tools-installer"* ]] && [[ $(dpkg-query -s google-android-build-tools-installer &>/dev/null ; echo $?) = 0 ]] && [[ "${MISSING[*]} " == *"aapt"* ]] && MISSING=( "${MISSING[@]/aapt/}" ) && rm /tmp/repair_depends/aapt*
-        
-        if [[ "${MISSING[*]} " == *"aapt"* ]] && [[ "${MISSING[*]} " == *"google-android-build-tools-installer"* ]]; then
-            echo -e "\n${RED}ATTENTION:${YELLOW} The following packages conflict with each other please select one to install:${BLUE}  \n"
-          
-            options=("aapt" "google-android-build-tools-installer" "help")                    
-            select opt in "${options[@]}"
-                do
-                    case $opt in
-                        "aapt")
-                            MISSING=( "${MISSING[@]/google-android-build-tools-installer/}" )
-                            rm /tmp/repair_depends/google-android-build-tools-installer*
-                            break
-                        ;;
-                        "google-android-build-tools-installer")
-                            MISSING=( "${MISSING[@]/aapt/}" )
-                            rm /tmp/repair_depends/aapt*
-                            break
-                        ;;
-                        "help")
-                            echo
-                            echo -e "${YELLOW}The package google-android-build-tools-installer is large and contains aapt in addition to other packages that may not be necessary to run apktool. We recomend just installing aapt itself.${BLUE}"
-                            echo
-                            sleep 2
-                        ;;
-                        *)
-                            echo
-                            echo -e "Invalid option please enter a valid numerical option"
-                        ;;
-                        esac
-                done  
-
-            #use for loop to parse errors
-            (dpkg -i /tmp/repair_depends/* &>/dev/null) &  
-            echo
-            PROG_MESSAGE="${YELLOW}Installing dependencies${RESTORE}"
-            COMP_MESSAGE="${YELLOW}Installed dependencies${RESTORE}"
-            spinLoader;echo 
-        fi              
+        (dpkg -i /tmp/repair_depends/* &>/dev/null) & 
+        echo 
+        PROG_MESSAGE="${YELLOW}Installing missing dependencies${RESTORE}"
+        COMP_MESSAGE="${YELLOW}Installed dependencies${RESTORE}"
+        spinLoader;echo              
     
         [[ -z ${MISSING[@]} ]] || echo -e "\n${YELLOW}The following dependencies were installed:\n" && sleep 2 && for depends in ${MISSING[@]}; do echo -e "${BLUE}$depends${RESTORE}"; done; sleep 2
         rm -r -f /tmp/repair_depends
     fi
+
+else 
+    sleep 0.5
+    echo -e "${RED}  [-] ${YELLOW}Java-Version 1.8 or greater is ${RED}Missing.${RESTORE}\n"    
+    mapfile -t java_depends < <(apt-cache depends apktool | grep "headless" | cut -d':' -f2| sed 's/^[ \t]*//;/<java8-runtime-headless>/d' |sort -u)
+    echo -e '${BLUE}Please select a ${YELLOW}\e[4mJava-Version\e[0m${BLUE} to be installed: \n${BLUE}'
+    select opt in "${java_depends[@]}"; do
+        if [ -z $opt ] || [ "$opt" == [a-zA-Z] ]; then
+            echo -e "${RED}ERROR: ${YELLOW}Invalid option please choose a valid numerical value from menu${BLUE}"
+        else
+            echo -e "\n${GREEN}Installing: ${YELLOW}$opt${RESTORE}"
+            apt-get install "$opt"
+            break
+        fi
+    
+    done
+fi
 
 # Upgrade and install latest version of apktool
 APKTOOL_UPGRADE() {
@@ -108,25 +106,25 @@ APKTOOL_UPGRADE() {
      axel -n 10 --output=/usr/bin/apktool.jar https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.4.0.jar &>/dev/null;
      [[ -e /usr/bin/apktool ]] && [[ -e /usr/bin/apktool.jar ]] && chmod +x /usr/bin/apktool /usr/bin/apktool.jar) &
     echo
-    PROG_MESSAGE="${YELLOW}Installing Apktool 2.4.0${RESTORE}"
-    COMP_MESSAGE="${YELLOW}Installed Apktool 2.4.0${RESTORE}"
-    spinLoader;echo 
+    PROG_MESSAGE="${YELLOW}Installing Apktool 2.4${RESTORE}"
+    COMP_MESSAGE="${YELLOW}Installed Apktool 2.4${RESTORE}"
+    spinLoader
     APKTOOL_VERSION
 }
 
 # Check apktool version
 APKTOOL_VERSION() {
-    echo -e "\n${YELLOW}Checking the version of Apktool you have installed.${RESTORE}"
+    echo -e "\n${BLUE}[-]${YELLOW} Checking the version of ${BLUE}Apktool${YELLOW} you have installed.${RESTORE}"
     sleep 1
-    verCheck=$(apktool --version | cut -d'.' -f2)
-    if [ $verCheck -lt 4 ]; then
-        echo -e "\n${RED}**** ${YELLOW}Apktool is not the latest version! ${RED}****${RESTORE}\n"
-        echo -e "\n${YELLOW}Removing Apktool version $(apktool --version) please wait...${RESTORE}"
+    verCheck=$(apktool --version 2>/dev/null | cut -d'.' -f2)
+    if [ $verCheck -lt 4 ] || [ $(apktool --version 2>/dev/null | grep "dirty") ]; then
+        echo -e "\n${RED}**** ${YELLOW}Apktool is not the correct version! ${RED}****${RESTORE}\n"
+        echo -e "${YELLOW}Removing Apktool version $(apktool --version 2>/dev/null) please wait...${RESTORE}"
         [[ -e /usr/bin/apktool ]] && rm -f /usr/bin/apktool
         [[ -e /usr/bin/apktool.jar ]] && rm -f /usr/bin/apktool.jar
         APKTOOL_UPGRADE   
     else
-        echo -e "\n${YELLOW}Apktool is ${GREEN}version 2.4.0${RESTORE}" 
+        echo -e "\n${YELLOW}  Apktool is the ${GREEN}Correct${YELLOW} version${RESTORE}" 
         sleep 1
     fi
 }
@@ -135,4 +133,3 @@ APKTOOL_VERSION() {
 [[ $(dpkg-query -s apktool &>/dev/null ; echo $?) = 0 ]] || [[ -e /usr/bin/apktool ]] && APKTOOL_VERSION || APKTOOL_UPGRADE
 
 # End
-echo -e "\n${GREEN}Apktool Fix Complete${RESTORE}\n"
